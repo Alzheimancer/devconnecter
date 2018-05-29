def2c3ea1de9844ce902d90f78e5666b603d507f
OOKA <ooka@OOKAs.local>
2018-05-29 20:08:42 +0700

    add User model

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,52 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
-router.get('/test', (req, res) => res.json({
-    msg: 'User works'
-}));
+// Load User model
+const User = require("../../models/User");
+
+router.get("/test", (req, res) =>
+    res.json({
+        msg: "User works"
+    })
+);
+
+router.post("/register", (req, res) => {
+    User.findOne({
+        email: req.body.email
+    }).then(user => {
+        if (user) {
+            return res.status(400).json({
+                email: "Email already exists"
+            });
+        } else {
+            const avatar = gravatar.url(req.body.email, {
+                s: "200", //Size
+                r: "pg", //Rating
+                d: "mm" //Default
+            });
+            console.log(avatar);
+
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                avatar,
+                password: req.body.password
+            });
+
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser
+                        .save()
+                        .then(user => res.json(user))
+                        .catch(err, console.log(err));
+                });
+            });
+        }
+    });
+});
 
 module.exports = router;
